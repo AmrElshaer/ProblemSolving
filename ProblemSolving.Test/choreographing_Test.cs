1d109974ba77b8ea24fb6dc1edbca8faaf35a7d3
Amr Elshaer <amrelsher07@gmail.com>
2022-07-23 17:32:29 +0200

    choreographing problem

---
+++ ProblemSolving.Test/choreographing_Test.cs
@@ -0,0 +1,15 @@
+﻿namespace ProblemSolving.Test
+{
+    public class choreographing_Test
+    {
+        [Theory]
+        [InlineData(0, 3, 4, 2, "YES")]
+        [InlineData(0, 2, 5, 3, "NO")]
+        [InlineData(0, 2, 5, 2, "NO")]
+        public void NumOfJump_ShouldReturnCorrectAnswer(int x1, int v1, int x2, int v2, string result)
+        {
+            var res = choreographing.kangaroo(x1, v1, x2, v2);
+            Assert.Equal(res, result);
+        }
+    }
+}
---
+++ ProblemSolving/choreographing.cs
@@ -0,0 +1,19 @@
+﻿namespace ProblemSolving
+{
+    public static class choreographing
+    {
+
+
+
+        public static string kangaroo(int x1, int v1, int x2, int v2)
+        {
+            if (v1 < v2) return "NO";
+            var disV = v1 - v2;
+            var disX = x2 - x1;
+            if (disX < disV || disV == 0 || disX % disV != 0) return "NO";
+            return "YES";
+
+        }
+
+    }
+}
