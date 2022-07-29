e8c0d13d7ba5a69cea0dc14b14e4daba9dabd44d
Amr Elshaer <amrelsher07@gmail.com>
2022-07-29 18:30:05 +0200

    Magice square

---
+++ ProblemSolving.Core/MagicSquare.cs
@@ -0,0 +1,67 @@
+﻿namespace ProblemSolving.Core
+{
+    public class MagicSquare
+    {
+        public static int formingMagicSquare(List<List<int>> s)
+        {
+            var allMaqiceSquare = new int[][][]
+            {
+                new int[][] {
+                new int[]{8, 1, 6 },
+                new int[]{3, 5, 7},
+                new int[]{4, 9, 2 }
+                },
+                new int[][]{
+                new int[]{6, 1, 8},
+                new int[]{7, 5, 3},
+                new int[]{2, 9, 4 }
+                },
+                new int[][]{
+                new int[]{4, 9, 2},
+                new int[]{3, 5, 7},
+                new int[]{8, 1, 6 }
+                },
+                new int[][]{
+                new int[]{2, 9, 4},
+                new int[]{7, 5, 3},
+                new int[]{6, 1, 8 }
+                },
+                new int[][]{
+
+                new int[]{8, 3, 4},
+                new int[]{1, 5, 9 },
+                new int[]{6, 7, 2}
+                },
+                new int[][]{
+                new int[]{4, 3, 8},
+                new int[]{9, 5, 1},
+                new int[]{2, 7, 6 }
+                },
+                new int[][]{
+                new int[]{6, 7, 2},
+                new int[]{1, 5, 9},
+                new int[]{8, 3, 4 }
+                },
+                new int[][]{
+                new int[]{2, 7, 6},
+                new int[]{9, 5, 1},
+                new int[]{4, 3, 8 }
+                }
+            };
+            int minCost = int.MaxValue;
+            foreach (var square in allMaqiceSquare)
+            {
+                int temp = 0;
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        temp += Math.Abs(square[i][j] - s[i][j]);
+                    }
+                }
+                minCost = Math.Min(minCost, temp);
+            }
+            return minCost;
+        }
+    }
+}
---
+++ ProblemSolving.Test/MagicSquare_Test.cs
@@ -0,0 +1,21 @@
+﻿using ProblemSolving.Core;
+
+namespace ProblemSolving.Test
+{
+    public class MagicSquare_Test
+    {
+        public static IEnumerable<object[]> TestGetSquare =>
+       new List<object[]>
+       {
+            new object[] { new List<List<int>> { new List<int> {5, 3 ,4 },
+            new List<int> { 1, 5 ,8 } , new List<int> { 6 ,4, 2}},7}
+       };
+        [Theory]
+        [MemberData(nameof(TestGetSquare))]
+        public void formingMagicSquare(List<List<int>> s, int minCost)
+        {
+            var res = MagicSquare.formingMagicSquare(s);
+            Assert.Equal(minCost, res);
+        }
+    }
+}
