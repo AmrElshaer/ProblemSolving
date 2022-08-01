78aaca9b89ab7a9ede7c0146b0f5ec7866424322
Amr Elshaer <amrelsher07@gmail.com>
2022-08-01 20:00:33 +0200

    add problem

---
+++ ProblemSolving.Core/BeautifulDaysAtMovies.cs
@@ -0,0 +1,28 @@
+﻿namespace ProblemSolving.Core
+{
+    public class BeautifulDaysAtMovies
+    {
+
+        public static int beautifulDays(int i, int j, int k)
+        {
+            return Enumerable.Range(i, j - i).Where(x => IsBeatifulDay(k, x)).Count();
+        }
+
+        private static bool IsBeatifulDay(int k, int start)
+        {
+            return Math.Abs(start - ReverseNumber(start)) % k == 0;
+        }
+
+        private static int ReverseNumber(int start)
+        {
+            string reverseNum = "";
+            var chars = start.ToString().Reverse();
+            foreach (var item in chars)
+            {
+                reverseNum += item;
+            }
+
+            return Convert.ToInt32(reverseNum);
+        }
+    }
+}
---
+++ ProblemSolving.Test/BeautifulDaysAtMovies_Test.cs
@@ -0,0 +1,15 @@
+﻿using ProblemSolving.Core;
+
+namespace ProblemSolving.Test
+{
+    public class BeautifulDaysAtMovies_Test
+    {
+        [Theory]
+        [InlineData(20, 23, 6, 2)]
+        public void CountBeautifulDaysAtMovies(int start, int end, int divisible, int res)
+        {
+            var numOfBeatifulDays = BeautifulDaysAtMovies.beautifulDays(start, end, divisible);
+            Assert.Equal(res, numOfBeatifulDays);
+        }
+    }
+}
