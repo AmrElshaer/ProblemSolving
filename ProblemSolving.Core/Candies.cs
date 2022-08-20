e829d4f714c8b0a09dc0700f8703aa6d97898567
Amr Elshaer <amrelsher07@gmail.com>
2022-08-20 23:54:01 +0200

    candies problem

---
+++ ProblemSolving.Core/Candies.cs
@@ -0,0 +1,34 @@
+﻿namespace ProblemSolving.Core
+{
+    public class Candies
+    {
+        public static long GetCandies(int n, List<int> arr)
+        {
+            int[] temp = Enumerable.Repeat(1, n).ToArray();
+            while (true)
+            {
+                bool changed = false;
+                for (int j = 1; j < n; j++)
+                {
+                    if (arr[j] > arr[j - 1] && temp[j - 1] >= temp[j])
+                    {
+                        temp[j] = temp[j - 1] + 1;
+                        changed = true;
+                    }
+
+                }
+                for (int j = n - 1; j > 0; j--)
+                {
+                    if (arr[j - 1] > arr[j] && temp[j - 1] <= temp[j])
+                    {
+                        temp[j - 1] = temp[j] + 1;
+                        changed = true;
+                    }
+                }
+                if (!changed) break;
+            }
+
+            return temp.Sum();
+        }
+    }
+}
---
+++ ProblemSolving.Test/Candies_Test.cs
@@ -0,0 +1,66 @@
+﻿using ProblemSolving.Core;
+
+namespace ProblemSolving.Test
+{
+    public class Candies_Test
+    {
+        public static IEnumerable<object[]> TestGetSquare =>
+            new List<object[]>
+            {
+                new object[] {6,new List<int>{4,6,4,5,6,2},10},
+                new object[] {3,new List<int>{1,2,2},4},
+                new object[]
+                {
+                    10, new List<int>
+                    {
+                        2,
+                        4,
+                        2,
+                        6,
+                        1,
+                        7,
+                        8,
+                        9,
+                        2,
+                        1
+                    },
+                    19
+                },
+                new object[]
+                {
+                    3,new List<int>(){3,2,1},6
+                },
+                new object[]
+                {
+                    2,new List<int>(){2,1},3
+                },new object[]
+                {
+                    2,new List<int>(){1,2},3
+                }
+                ,new object[]
+                {
+                    1,new List<int>(){2},1
+                },new object[]
+                {
+                    8,new List<int>(){2,
+                        4,
+                        3,
+                        5,
+                        2,
+                        6,
+                        4,
+                        5},12
+                }
+            };
+        [Theory]
+        [MemberData(nameof(TestGetSquare))]
+
+        public void GetMinimumCandies(int n, List<int> arr, int res)
+        {
+            long result = Candies.GetCandies(n, arr);
+            Assert.Equal(res, result);
+        }
+    }
+
+
+}
