b2c1bc5500887630d29731cbfb374b9710cb7054
Amr Elshaer <amrelsher07@gmail.com>
2022-08-21 21:19:33 +0200

    add PriyankaToys Problem

---
+++ ProblemSolving.Core/PriyankaToys.cs
@@ -0,0 +1,22 @@
+﻿namespace ProblemSolving.Core
+{
+    public class PriyankaToys
+    {
+        public static int GetNumberOfContainer(List<int> weight)
+        {
+            var sortedWeight = weight.OrderBy(x => x).ToList();
+            var numberOfContainer = 1;
+            var maxUnite = 4;
+            var range = sortedWeight[0] + maxUnite;
+            foreach (var item in sortedWeight)
+            {
+                if (item > range)
+                {
+                    range = item + maxUnite;
+                    numberOfContainer++;
+                }
+            }
+            return numberOfContainer;
+        }
+    }
+}
---
+++ ProblemSolving.Test/PriyankaToys_Test.cs
@@ -0,0 +1,27 @@
+﻿using ProblemSolving.Core;
+
+namespace ProblemSolving.Test
+{
+    public class PriyankaToys_Test
+    {
+        public static IEnumerable<object[]> TestGetNumberOfContainer =>
+            new List<object[]>
+            {
+                new object[]
+                {
+                    new List<int>{ 1 ,2 ,3 ,21 ,7 ,12 ,14 ,21 }
+                    ,
+                    4
+                }
+            };
+        [Theory]
+        [MemberData(nameof(TestGetNumberOfContainer))]
+        public void GetNumberOfContainer(List<int> weight, int minCost)
+        {
+            int numOfContainer = PriyankaToys.GetNumberOfContainer(weight);
+            Assert.Equal(minCost, numOfContainer);
+        }
+    }
+
+
+}
