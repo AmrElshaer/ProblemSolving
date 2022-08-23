15516c969cdc3f0cc4b87367de35d7165114c47f
Amr Elshaer <amrelsher07@gmail.com>
2022-08-23 14:45:27 +0200

    add largePermutation Problem

---
+++ ProblemSolving.Core/LargestPermutation.cs
@@ -0,0 +1,49 @@
+﻿namespace ProblemSolving.Core
+{
+    public class LargestPermutation
+    {
+        public static List<int> LargestPermutationNumList(int k, List<int> arr)
+        {
+            var data = KeyValueData(arr);
+            int numOfSwap = 0, i = 0, n = arr.Count;
+
+            while (numOfSwap < k && i < arr.Count)
+            {
+                if (arr[i] < n)
+                {
+                    var index = data[n];
+                    arr = SwapBaseData(arr, i, index);
+                    data = SwapKeyValueData(arr, data, i, index);
+                    numOfSwap++;
+                }
+
+                n -= 1;
+                i++;
+            }
+            return arr;
+        }
+
+        private static Dictionary<int, int> SwapKeyValueData(List<int> arr, Dictionary<int, int> data, int i, int index)
+        {
+            (data[arr[i]], data[arr[index]]) = (data[arr[index]], data[arr[i]]);
+            return data;
+        }
+
+        private static List<int> SwapBaseData(List<int> arr, int i, int index)
+        {
+            (arr[i], arr[index]) = (arr[index], arr[i]);
+            return arr;
+        }
+
+        private static Dictionary<int, int> KeyValueData(List<int> arr)
+        {
+            var data = new Dictionary<int, int>();
+            for (int j = 0; j < arr.Count; j++)
+            {
+                data[arr[j]] = j;
+            }
+
+            return data;
+        }
+    }
+}
---
+++ ProblemSolving.Test/LargestPermutation_Test.cs
@@ -0,0 +1,65 @@
+﻿using ProblemSolving.Core;
+
+namespace ProblemSolving.Test
+{
+    public class LargestPermutation_Test
+    {
+        public static IEnumerable<object[]> TestLargePermutation =>
+            new List<object[]>
+            {
+                new object[]
+                {
+                    new List<int>{ 1 ,2 ,3 ,4}
+                    ,
+                    1,
+                    new List<int>{ 4 ,2 ,3 ,1}
+                },
+                new object[]
+                {
+                    new List<int>{ 4, 2 ,3 ,5 ,1 }
+                    ,
+                    1,
+                    new List<int>{ 5, 2 ,3 ,4 ,1 }
+                },
+                new object[]
+                {
+                    new List<int>{ 2,1 }
+                    ,
+                    1,
+                    new List<int>{ 2,1 }
+                }
+            };
+        [Theory]
+        [MemberData(nameof(TestLargePermutation))]
+        public void GetLargePermutationArr_ValidInput_Success(List<int> arr, int minSwap, List<int> result)
+        {
+            var res = LargestPermutation.LargestPermutationNumList(minSwap, arr);
+            Assert.Equal(result, res);
+        }
+        public static IEnumerable<object[]> TestLargePermutationNOtValid =>
+            new List<object[]>
+            {
+                new object[]
+                {
+                    new List<int>{ 1 ,2 ,3 ,4}
+                    ,
+                    1,
+                    new List<int>{ 1 ,2 ,4 ,3}
+                },
+                new object[]
+                {
+                    new List<int>{ 4, 2 ,3 ,5 ,1 }
+                    ,
+                    1,
+                    new List<int>{ 2, 5 ,3 ,4 ,1 }
+                }
+            };
+        [Theory]
+        [MemberData(nameof(TestLargePermutationNOtValid))]
+        public void GetLargePermutationArr_NotValidInput_Success(List<int> arr, int minSwap, List<int> result)
+        {
+            var res = LargestPermutation.LargestPermutationNumList(minSwap, arr);
+            Assert.NotEqual(result, res);
+        }
+    }
+}
