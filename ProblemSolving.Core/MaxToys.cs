76852714e3a9df46a2b6a574baa4d7743f6aa839
Amr Elshaer <amrelsher07@gmail.com>
2022-08-27 14:35:43 +0200

    add maxToys Problem

---
+++ ProblemSolving.Core/MaxToys.cs
@@ -0,0 +1,36 @@
+﻿namespace ProblemSolving.Core
+{
+    public class MaxToys
+    {
+        public static int maximumToys(List<int> prices, int k)
+        {
+            var res = prices.OrderBy(p => p)
+                .CountAggregateWhile((init, curr) => init + curr,
+                    c => c <= k);
+            return res;
+        }
+    }
+
+    public static class LinqExtension
+    {
+        public static int CountAggregateWhile<TSource>(this IEnumerable<TSource> source,
+            Func<TSource, TSource, TSource> func,
+            Func<TSource, bool> predicate)
+        {
+            using (IEnumerator<TSource> e = source.GetEnumerator())
+            {
+                TSource result = e.Current;
+                TSource tmp = default(TSource);
+                int count = 0;
+                while (e.MoveNext() && predicate(tmp = func(result, e.Current)))
+                {
+                    result = tmp;
+                    count++;
+                }
+
+                return count;
+            }
+        }
+    }
+
+}
---
+++ ProblemSolving.Test/MaxToys_Test.cs
@@ -0,0 +1,26 @@
+﻿using ProblemSolving.Core;
+
+namespace ProblemSolving.Test
+{
+    public class MaxToys_Test
+    {
+        public static IEnumerable<object[]> TestGetMaxNumberOfToys =>
+            new List<object[]>
+            {
+                new object[]
+                {
+                    new List<int>{ 1, 12, 5, 111 ,200 ,1000 ,10 }
+                    ,
+                    50,
+                    4
+                }
+            };
+        [Theory]
+        [MemberData(nameof(TestGetMaxNumberOfToys))]
+        public void GetMaxNumberOfToys(List<int> prices, int k, int res)
+        {
+            int result = MaxToys.maximumToys(prices, k);
+            Assert.Equal(res, result);
+        }
+    }
+}
