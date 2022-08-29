3f36296fa94733525e4da534f42549cab83fad57
Amr Elshaer <amrelsher07@gmail.com>
2022-08-29 16:42:35 +0200

    add NewYearChaos Problem

---
+++ ProblemSolving.Core/NewYearChaos.cs
@@ -0,0 +1,47 @@
+﻿namespace ProblemSolving.Core
+{
+    public class NewYearChaos
+    {
+        public static string GetNumberOfBribes(List<int> q)
+        {
+            int n = q.Count;
+
+            int x, y = 0;
+
+            for (int j = 0; j < n; j++)
+            {
+
+
+                if (q[j] - j > 3)
+                {
+                    y = -1;
+                }
+            }
+
+            if (y == -1)
+            {
+                return "Too chaotic";
+            }
+
+            int yr;
+            for (int k = 0; k < n; k++)
+            {
+                yr = y;
+                for (int j = 0; j < n - 1; j++)
+                {
+                    if (q[j] > q[j + 1])
+                    {
+                        x = q[j];
+                        q[j] = q[j + 1];
+                        q[j + 1] = x;
+                        y++;
+                    }
+                }
+                if (yr == y)
+                    break;
+            }
+
+            return y.ToString();
+        }
+    }
+}
---
+++ ProblemSolving.Test/NewYearChaos_Test.cs
@@ -0,0 +1,32 @@
+﻿using ProblemSolving.Core;
+
+namespace ProblemSolving.Test
+{
+    public class NewYearChaos_Test
+    {
+        public static IEnumerable<object[]> TestGetNumberOfBribes =>
+            new List<object[]>
+            {
+                new object[]
+                {
+                    new List<int>{ 2, 1, 5, 3,4 }
+                    ,
+                    "3",
+
+                },new object[]
+                {
+                    new List<int>{ 2, 5, 1, 3 ,4 }
+                    ,
+                    "Too chaotic",
+
+                }
+            };
+        [Theory]
+        [MemberData(nameof(TestGetNumberOfBribes))]
+        public void GetNumberOfBribes(List<int> q, string expected)
+        {
+            var numberOfBribes = NewYearChaos.GetNumberOfBribes(q);
+            Assert.Equal(expected, numberOfBribes);
+        }
+    }
+}
