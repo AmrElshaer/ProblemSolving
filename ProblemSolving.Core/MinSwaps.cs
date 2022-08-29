e4d4107fdb8ea52c40d572d56ff108f899969077
Amr Elshaer <amrelsher07@gmail.com>
2022-08-29 20:53:19 +0200

    add MinNumberOfSwap Problem

---
+++ ProblemSolving.Core/MinSwaps.cs
@@ -0,0 +1,42 @@
+﻿namespace ProblemSolving.Core
+{
+    public class MinSwaps
+    {
+        public static int MinimumSwaps(int[] arr)
+        {
+
+            var visited = new bool[arr.Length];
+            var modeMap = new Dictionary<int, int>();
+            for (int i = 1; i <= arr.Length; i++)
+            {
+                modeMap.Add(i, arr[i - 1]);
+            }
+
+            int numOfSwap = 0;
+            for (int i = 1; i <= arr.Length; i++)
+            {
+                if (!visited[i - 1])
+                {
+                    visited[i - 1] = true;
+                    if (modeMap[i] == i)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        int c = modeMap[i];
+                        while (!visited[c - 1])
+                        {
+                            visited[c - 1] = true;
+                            var nextNode = modeMap[c];
+                            c = nextNode;
+                            numOfSwap++;
+                        }
+                    }
+                }
+
+            }
+            return numOfSwap;
+        }
+    }
+}
---
+++ ProblemSolving.Test/MinSwaps_Test.cs
@@ -0,0 +1,30 @@
+﻿using ProblemSolving.Core;
+
+namespace ProblemSolving.Test
+{
+    public class MinSwaps_Test
+    {
+        public static IEnumerable<object[]> TestGetMinNumberOfSwap =>
+            new List<object[]>
+            {
+                new object[]
+                {
+                    new int[]{ 1,4,3,2 }
+                    ,1
+
+                },new object[]
+                {
+                    new int[]{ 4, 3 ,1 ,2 }
+                    ,3
+
+                }
+            };
+        [Theory]
+        [MemberData(nameof(TestGetMinNumberOfSwap))]
+        public void GetNumberOfSwap(int[] q, int expected)
+        {
+            var minimumSwaps = MinSwaps.MinimumSwaps(q);
+            Assert.Equal(expected, minimumSwaps);
+        }
+    }
+}
