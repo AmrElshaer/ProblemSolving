0efb49da4c7bfd7f7dfdcdd9feae5476f99de4b5
Amr Elshaer <amrelsher07@gmail.com>
2022-09-09 14:08:52 +0200

    add new problem ShareTwoString

---
+++ ProblemSolving.Core/ShareTwoString.cs
@@ -0,0 +1,21 @@
+﻿namespace ProblemSolving.Core
+{
+    public class ShareTwoString
+    {
+
+        public static string TwoStrings(string s1, string s2)
+        {
+            var uniqueFirst = s1.Distinct().ToDictionary(i => i, i => true);
+            var uniqueSecond = s2.Distinct();
+            foreach (var t in uniqueSecond)
+            {
+                if (uniqueFirst.TryGetValue(t, out bool value))
+                {
+                    return "YES";
+                }
+            }
+
+            return "NO";
+        }
+    }
+}
---
+++ ProblemSolving.Test/ShareTwoString_Test.cs
@@ -0,0 +1,31 @@
+﻿using ProblemSolving.Core;
+
+namespace ProblemSolving.Test
+{
+    public class ShareTwoString_Test
+    {
+        public static IEnumerable<object[]> TestShareTwoStrings =>
+            new List<object[]>
+            {
+                new object[]
+                {
+                    "hello",
+                    "world",
+                    "YES"
+                },
+                new object[]
+                {
+                    "hi",
+                    "world",
+                    "NO"
+                }
+            };
+        [Theory]
+        [MemberData(nameof(TestShareTwoStrings))]
+        public void TwoStrings(string s1, string s2, string res)
+        {
+            var result = ShareTwoString.TwoStrings(s1, s2);
+            Assert.Equal(res, result);
+        }
+    }
+}
